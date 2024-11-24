d1c19075b16c47750bafbc4067717c4c7aa0618d
Cainã Oliveira <161257846+kronos164@users.noreply.github.com>
2024-11-24 21:04:03 +0000

    Add feature (keep only last 10 timestamps entries)

--- weather.sh
+++ weather.sh
@@ -2,10 +2,25 @@
 
 echo "Downloading weather data"
 curl -o data/weather/`date +"%Y%m%d_%H%M%S_athenry.json"` https://prodapi.metweb.ie/observations/athenry/today
+
 echo "Download complete"
+
+# Keep only the last 10 JSON files in the directory
+echo "Cleaning up old JSON files"
+ls -tp data/weather/*.json | grep -v '/$' | tail -n +11 | xargs -d '\n' rm -- 2>/dev/null
+
+echo "Old JSON files removed"
+
 echo "Updating timestamps file"
 date +%Y%m%d_%H%M%S_%Z >> data/timestamps/now.txt
+
+# Keep only the last 10 lines in now.txt
+tail -n 10 data/timestamps/now.txt > data/timestamps/temp.txt
+mv data/timestamps/temp.txt data/timestamps/now.txt
+
 echo "Timestamps updated"
-echo "Reading up to date timestamps"
+
+echo "Reading last 10 timestamps"
 more data/timestamps/now.txt
+
 echo "Script finished"
